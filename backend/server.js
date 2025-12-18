933e7c32e80ac0d143c1f2528de99f08e428b32e
vanessa <vane.villatoro010306@gmail.com>
2025-12-17 18:49:53 -0600

    modificacion en el backend

--- backend/server.js
+++ backend/server.js
@@ -198,4 +198,4 @@ app.get('/api/export-plan/:id/docx', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${5000}`));
