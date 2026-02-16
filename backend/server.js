911ba5bac092bcc6e8cdd0b5f670d3c490a52088
vanessa <vane.villatoro010306@gmail.com>
2026-02-15 23:28:59 -0600

    cambios nuevos

--- backend/server.js
+++ backend/server.js
@@ -405,3 +405,5 @@ app.post('/api/auth/login', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Servidor en puerto ${PORT}`));
+
+module.exports = app;
