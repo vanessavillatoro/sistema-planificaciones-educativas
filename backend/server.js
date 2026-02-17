e05db6655956247a12dbb4a7ab272427ef195dbc
vanessa <vane.villatoro010306@gmail.com>
2026-02-17 13:54:00 -0600

    cambios en server.js

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options('/api/(.*)', cors());
+app.options('/api/:path*', cors());
 // --- FIN DEL CAMBIO ---
 
 app.use(express.json());
