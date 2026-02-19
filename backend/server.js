dd1b69ee20da8e44886f0ed8ffc79d62f83e09a0
vanessa <vane.villatoro010306@gmail.com>
2026-02-19 17:00:16 -0600

    server.js

--- backend/server.js
+++ backend/server.js
@@ -421,6 +421,6 @@ app.post('/api/auth/login', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Servidor en puerto ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 puerto ${PORT}`));
 
 module.exports = app;
